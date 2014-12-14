6789f75802c1553a48b495288e5148f432a667f5
mghosh4 <mghosh4@illinois.edu>
2014-12-14 19:45:43 +0000

    Some more scripts

---
+++ Scripts/mongoShard/gitClone.sh
@@ -0,0 +1,15 @@
+source $1
+
+GIT_STRING="git clone http://github.com/mainak/EmulabScripts"
+REMOVE_STRING="sudo rm -rf /home/mainak/EmulabScripts/"
+for  node in ${QUERY_ROUTERS//,/ }
+do
+        echo "clone git hub to $node ... $REMOVE_STRING"
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+            $REMOVE_STRING"
+         echo "clone git hub to $node ... $GIT_STRING"
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+            cd /home/mainak &&
+            $GIT_STRING" 
+done
+echo ""
